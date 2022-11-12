5b0ac189b55c97f8e56e98f010bfdc7cd7a4c29c
kido-kriday <87981846+kido-kriday@users.noreply.github.com>
2022-11-12 20:28:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+function start() 
+{
+  document.getElementById("textbox").innerHTML = "";
+  recognition.start();
+}
+
+recognition.onresult = function run (event) {
+  console.log(event);
+  var Content = event.results[0][0].transcript;
+  document.getElementById("textbox").innerHTML=Content;
+  console.log(Content);
+  if(Content=="he is a clever man indeed")
+  DelayNode
+  DelayNode
+  DelayNode
+  DelayNode
+  
+  document.getElementById("le_le").play();
+};
+
+recognition.onresult = function(event) {
+  console.log(event);
+  var Content = event.results[0][0].transcript;
+  document.getElementById("textbox").innerHTML=Content;
+  console.log(Content);
+  if(Content=="he is a clever man indeed")
+  { console.log("taking selfie--");
+    speak();
+  };
+function speak()
+}
